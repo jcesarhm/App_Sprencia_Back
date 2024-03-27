ebb16e21598419a548f9136c6846b1c272115d5c
DESKTOP-9NPKDB6\jcesa <julio.cesar.he@icloud.com>
2024-03-27 21:35:39 +0100

    Add project files.

---
+++ Sprencia_Api/SprenciaDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SPRENCIA.Domain.Models;
+
+
+namespace SPRENCIA.Infraestructure
+{
+    public class SprenciaDbContext: DbContext
+    {
+         public SprenciaDbContext(DbContextOptions<SprenciaDbContext> options) : base(options) 
+        { 
+
+        }
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<Schedule> Schedules { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+    }
+}
